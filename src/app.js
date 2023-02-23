a0925a66380da9736350954664a183c6121a9a97
bona1204 <sebastian.bonillad@gmail.com>
2023-02-23 18:33:59 +0000

    ok

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import "bootstrap";
 import "./style.css";
 
+//si siempre se repite una parte como [Math.floor(Math.random() * 3) es mejor hacerla una función//
+const reload = document.getElementById("reload");
+reload.addEventListener("click", _ => {
+  location.reload();
+});
+function random_word(words) {
+  return words[Math.floor(Math.random() * 3)];
+}
+
 function generateexcuse() {
   let excuses = {
     guilt: ["My dog", "My brother", "My uncle", "My mom"],
@@ -8,14 +17,16 @@ function generateexcuse() {
     item: ["my homework", "my project", "my tesis", "my report"],
     time: ["yesterday", "an hour ago", "this morning", "the past week"]
   };
-  let Guilt = excuses.guilt[Math.round(Math.random() * 3)];
-  let Action = excuses.action[Math.round(Math.random() * 3)];
-  let Item = excuses.item[Math.round(Math.random() * 3)];
-  let Time = excuses.time[Math.round(Math.random() * 3)];
+
+  let Guilt = random_word(excuses.guilt);
+  let Action = random_word(excuses.action);
+  let Item = random_word(excuses.item);
+  let Time = random_word(excuses.time);
 
   return Guilt + " " + Action + " " + Item + " " + Time + " ";
 }
 
+//Mejor crear un boton para generar una excusa en vez de recargar la pagina
 window.onload = function() {
   document.getElementById("excuse").innerHTML = generateexcuse();
 };
